6fb288b711f8ec2fb361a2743098854128ef781e
harishankarsomasundaram <harishankar.info@gmail.com>
2014-08-20 20:01:16 +0530

    Added Basic Class Libraries

---
+++ Askmentor.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Askmentor.WebUI.MvcApplication" Language="C#" %>
---
+++ Askmentor.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Askmentor.WebUI.Startup))]
+namespace Askmentor.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
